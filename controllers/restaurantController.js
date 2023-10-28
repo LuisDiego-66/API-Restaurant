5505398dd45cf352cef512fd551dc8bf4d530ad0
LuisDiego-66 <luisdiegoborja8@gmail.com>
2023-10-28 13:24:24 -0400

    add tryCatch

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -21,69 +21,86 @@ export const createMesa = async (req, res) => {
   const { numero, capacidad, auxiliar, seccion, mesas } = req.body;
   let estado = "Libre";
 
-  if (auxiliar && mesas != null) {
-    estado = "Ocupado";
-    const mesa = await Mesa.create({
-      numero,
-      capacidad,
-      auxiliar,
-      seccion,
-      estado,
-      mesas,
-    });
-    res.json(mesa);
-  } else {
-    const mesa = await Mesa.create({
-      numero,
-      capacidad,
-      auxiliar,
-      seccion,
-      estado,
-    });
-    res.json(mesa);
+  try {
+    if (auxiliar && mesas != null) {
+      estado = "Ocupado";
+      const mesa = await Mesa.create({
+        numero,
+        capacidad,
+        auxiliar,
+        seccion,
+        estado,
+        mesas,
+      });
+      res.json(mesa);
+    } else {
+      const mesa = await Mesa.create({
+        numero,
+        capacidad,
+        auxiliar,
+        seccion,
+        estado,
+      });
+      res.json(mesa);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
 export const getMesas = async (req, res) => {
-  const mesas = await Mesa.findAll();
-  res.json(mesas);
+  try {
+    const mesas = await Mesa.findAll();
+    res.json(mesas);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const cambiarEstadoMesa = async (req, res) => {
   const { id } = req.params;
   const { estado } = req.body;
-  const mesa = await Mesa.findByPk(id);
+  try {
+    const mesa = await Mesa.findByPk(id);
 
-  if (estado == "1") {
-    mesa.set({ estado: "Libre" });
-    await mesa.save();
-    console.log("se cambio el estado a Libre");
+    if (estado == "1") {
+      mesa.set({ estado: "Libre" });
+      await mesa.save();
+      console.log("se cambio el estado a Libre");
+    }
+    if (estado == "2") {
+      mesa.set({ estado: "Ocupado" });
+      await mesa.save();
+      console.log("se cambio el estado a Ocupado");
+    }
+    res.send("Changed Status Successfully");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  if (estado == "2") {
-    mesa.set({ estado: "Ocupado" });
-    await mesa.save();
-    console.log("se cambio el estado a Ocupado");
-  }
-  res.send("Changed Status Successfully");
 };
 
 export const cambiarEstadoMesas = async (req, res) => {
   const { tableAux, tablesIds } = req.body;
-  const mesaAux = await Mesa.findByPk(tableAux);
 
-  tablesIds.forEach(async (table) => {
-    const mesa = await Mesa.findByPk(table.id);
+  try {
+    const mesaAux = await Mesa.findByPk(tableAux);
 
-    if (mesa.estado == "Libre") {
-      mesa.set({ estado: "Ocupado" });
-      await mesa.save();
-    } else if (mesa.estado == "Ocupado") {
-      mesa.set({ estado: "Libre" });
-      mesaAux.set({ estado: "Deshabilitado" });
-      await Promise.all([mesa.save(), mesaAux.save()]);
-    }
-  });
-  res.send("Changed Status Successfully");
+    tablesIds.forEach(async (table) => {
+      const mesa = await Mesa.findByPk(table.id);
+
+      if (mesa.estado == "Libre") {
+        mesa.set({ estado: "Ocupado" });
+        await mesa.save();
+      } else if (mesa.estado == "Ocupado") {
+        mesa.set({ estado: "Libre" });
+        mesaAux.set({ estado: "Deshabilitado" });
+        await Promise.all([mesa.save(), mesaAux.save()]);
+      }
+    });
+    res.send("Changed Status Successfully");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 //Categorias
@@ -117,80 +134,113 @@ export const createCategoria = async (req, res) => {
 };
 
 export const getCategorias = async (req, res) => {
-  const categorias = await Categoria.findAll();
-  res.json(categorias);
+  try {
+    const categorias = await Categoria.findAll();
+    res.json(categorias);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getComidaCategoria = async (req, res) => {
   const { id } = req.params;
-  const comidas = await Comida.findAll({
-    where: { categoriaId: id },
-    include: {
-      model: Variante,
-      include: { model: Opcion },
-    },
-  });
-  res.json(comidas);
+
+  try {
+    const comidas = await Comida.findAll({
+      where: { categoriaId: id },
+      include: {
+        model: Variante,
+        include: { model: Opcion },
+      },
+    });
+    res.json(comidas);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 //Opciones
 export const createOpcion = async (req, res) => {
   const { nombre, precio, varianteId } = req.body;
-  await Opcion.create({
-    nombre,
-    precio,
-    varianteId,
-  });
-  res.send("ok");
+
+  try {
+    await Opcion.create({
+      nombre,
+      precio,
+      varianteId,
+    });
+    res.send("ok");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getOpciones = async (req, res) => {
-  const opciones = await Opcion.findAll();
-  res.json(opciones);
+  try {
+    const opciones = await Opcion.findAll();
+    res.json(opciones);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 //Variantes
 export const createVariante = async (req, res) => {
   const { tamaño, estado } = req.body;
-  await Variante.create({
-    tamaño,
-    estado,
-  });
-  res.send("ok");
+
+  try {
+    await Variante.create({
+      tamaño,
+      estado,
+    });
+    res.send("ok");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getVariantes = async (req, res) => {
-  const variantes = await Variante.findAll();
-  res.json(variantes);
+  try {
+    const variantes = await Variante.findAll();
+    res.json(variantes);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 //Items
 export const getItems = async (req, res) => {
-  const item = await Item.findAll();
-  res.json(item);
+  try {
+    const item = await Item.findAll();
+    res.json(item);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const createItem = async (req, res) => {
   const { id: ordenId } = req.params;
   const { cantidad, subtotal, comidaId, opciones } = req.body;
-
-  const item = await Item.create({
-    cantidad,
-    subtotal,
-    comidaId,
-    ordenId,
-  });
-  opciones.forEach(async (opcion) => {
-    await Item_Opcion.create({
-      itemId: item.id,
-      opcioneId: opcion.id,
+  try {
+    const item = await Item.create({
+      cantidad,
+      subtotal,
+      comidaId,
+      ordenId,
     });
-  });
-  res.send("ok");
+    opciones.forEach(async (opcion) => {
+      await Item_Opcion.create({
+        itemId: item.id,
+        opcioneId: opcion.id,
+      });
+    });
+    res.send("ok");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 //Comidas
-
 export const createComidaNormal = async (req, res) => {
   const { nombre, descripcion, precio, categoriaId } = req.body;
 
@@ -268,24 +318,33 @@ export const createComida = async (req, res) => {
 };
 
 export const getComidas = async (req, res) => {
-  const comidas = await Comida.findAll({
-    include: {
-      model: Variante,
-      include: { model: Opcion },
-    },
-  });
-  res.json(comidas);
+  try {
+    const comidas = await Comida.findAll({
+      include: {
+        model: Variante,
+        include: { model: Opcion },
+      },
+    });
+    res.json(comidas);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getComida = async (req, res) => {
   const { id } = req.params;
-  const comidas = await Comida.findByPk(id, {
-    include: {
-      model: Variante,
-      include: { model: Opcion },
-    },
-  });
-  res.json(comidas);
+
+  try {
+    const comidas = await Comida.findByPk(id, {
+      include: {
+        model: Variante,
+        include: { model: Opcion },
+      },
+    });
+    res.json(comidas);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 //Ordenes
@@ -294,113 +353,148 @@ export const createOrden = async (req, res) => {
 
   const waiter = req.waiter;
   const waiterId = waiter.id;
-
-  const orden = await Orden.create({
-    total,
-    mesaId,
-    waiterId,
-  });
-
-  items.forEach(async (item) => {
-    const i = await Item.create({
-      cantidad: item.cantidad,
-      subtotal: item.subtotal,
-      comidaId: item.comidaId,
-      ordenId: orden.id,
+  try {
+    const orden = await Orden.create({
+      total,
+      mesaId,
+      waiterId,
     });
-    item.opciones.forEach(async (opcion) => {
-      await Item_Opcion.create({
-        itemId: i.id,
-        opcioneId: opcion.id,
+
+    items.forEach(async (item) => {
+      const i = await Item.create({
+        cantidad: item.cantidad,
+        subtotal: item.subtotal,
+        comidaId: item.comidaId,
+        ordenId: orden.id,
+      });
+      item.opciones.forEach(async (opcion) => {
+        await Item_Opcion.create({
+          itemId: i.id,
+          opcioneId: opcion.id,
+        });
       });
     });
-  });
-  res.send("ok");
+    res.send("ok");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getOrdenes = async (req, res) => {
-  const ordenes = await Orden.findAll({
-    include: [
-      { model: Mesa },
-      { model: Item, include: [{ model: Comida }, { model: Opcion }] },
-    ],
-  });
-  res.json(ordenes);
+  try {
+    const ordenes = await Orden.findAll({
+      include: [
+        { model: Mesa },
+        { model: Item, include: [{ model: Comida }, { model: Opcion }] },
+      ],
+    });
+    res.json(ordenes);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getOrden = async (req, res) => {
   const { id } = req.params;
-  const ordenes = await Orden.findByPk(id, {
-    include: [
-      { model: Mesa },
-      { model: Item, include: [{ model: Comida }, { model: Opcion }] },
-    ],
-  });
-  res.json(ordenes);
+
+  try {
+    const ordenes = await Orden.findByPk(id, {
+      include: [
+        { model: Mesa },
+        { model: Item, include: [{ model: Comida }, { model: Opcion }] },
+      ],
+    });
+    res.json(ordenes);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const cambiarEstadoOrden = async (req, res) => {
   const { id } = req.params;
   const { estado } = req.body;
-  const orden = await Orden.findByPk(id);
 
-  if (estado == "1") {
-    orden.set({ estado: "Entregado" });
-    await orden.save();
+  try {
+    const orden = await Orden.findByPk(id);
+
+    if (estado == "1") {
+      orden.set({ estado: "Entregado" });
+      await orden.save();
+    }
+    if (estado == "2") {
+      orden.set({ estado: "Cancelado" });
+      await orden.save();
+    }
+    res.send("Changed Status Successfully");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  if (estado == "2") {
-    orden.set({ estado: "Cancelado" });
-    await orden.save();
-  }
-  res.send("Changed Status Successfully");
 };
 
 export const cambiarMesaOrden = async (req, res) => {
   const { id } = req.params;
   const { mesaId } = req.body;
-
-  const orden = await Orden.findByPk(id);
-  orden.set({ mesaId: mesaId });
-  await orden.save();
-  res.send("ok");
+  try {
+    const orden = await Orden.findByPk(id);
+    orden.set({ mesaId: mesaId });
+    await orden.save();
+    res.send("ok");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const cambiarWaiterOrden = async (req, res) => {
   const { id } = req.params;
   const { waiterId } = req.body;
-
-  const orden = await Orden.findByPk(id);
-  orden.set({ waiterId: waiterId });
-  await orden.save();
-  res.send("ok");
+  try {
+    const orden = await Orden.findByPk(id);
+    orden.set({ waiterId: waiterId });
+    await orden.save();
+    res.send("ok");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getAllWaiter = async (req, res) => {
-  const waiters = await Waiter.findAll();
-  res.json(waiters);
+  try {
+    const waiters = await Waiter.findAll();
+    res.json(waiters);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getWaiter = async (req, res) => {
   const waiter = req.waiter;
   const id = waiter.id;
-  const w = await Waiter.findByPk(id);
-  res.json(w);
+
+  try {
+    const w = await Waiter.findByPk(id);
+    res.json(w);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const tableFromWaiter = async (req, res) => {
   const waiter = req.waiter;
   const id = waiter.id;
-
-  const orden = await Orden.findAll({
-    //attributes: ['mesaId'],
-    where: {
-      waiterId: id,
-      estado: "Pendiente",
-    },
-    include: [
-      { model: Mesa },
-      { model: Item, include: [{ model: Comida }, { model: Opcion }] },
-    ],
-  });
-  res.json(orden);
+  try {
+    const orden = await Orden.findAll({
+      //attributes: ['mesaId'],
+      where: {
+        waiterId: id,
+        estado: "Pendiente",
+      },
+      include: [
+        { model: Mesa },
+        { model: Item, include: [{ model: Comida }, { model: Opcion }] },
+      ],
+    });
+    res.json(orden);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
