e41a8f7b48e9f08ef87c64f024ae8e354eddce75
LuisDiego-66 <luisdiegoborja8@gmail.com>
2023-10-30 19:00:42 -0400

    add image to waiter

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,18 +17,36 @@ export const register = async (req, res) => {
       return res.status(500).json({ error: "pin or email already exists" });
     }
 
-    const passwordHash = await bcrypt.hash(password, 10);
-    const newWaiter = new Waiter({
-      nombre,
-      password: passwordHash,
-      correo,
-      pin,
-      celular,
-      direccion,
-    });
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+    await cloudinary.uploader.upload(req.file.path, async (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: false,
+          message: "error",
+        });
+      }
+      const url = result.url;
 
-    await newWaiter.save();
-    res.send("waiter created");
+      const passwordHash = await bcrypt.hash(password, 10);
+      const newWaiter = new Waiter({
+        nombre,
+        password: passwordHash,
+        correo,
+        pin,
+        celular,
+        direccion,
+        url,
+      });
+
+      await newWaiter.save();
+      res.status(200).json({
+        success: true,
+        message: "Uploaded successfull",
+      });
+    });
   } catch (error) {
     console.log(error);
   }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { register, login, loginPin } from "../controllers/authController.js";
+import upload from "../middlewares/subirImagen.js";
+
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", upload.single("imagen"), register);
 
 router.post("/login", login);
 router.post("/login-pin", loginPin);
